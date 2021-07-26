4fa201b404d9a5388f91d1f55746add984fc2065
andrew <klimichkartorgnusov@gmail.com>
2021-07-26 22:38:27 +0300

    MappedSuperclassMapping no longer extends EntityMapping

--- src/Mapping/MappedSuperclassMapping.php
+++ src/Mapping/MappedSuperclassMapping.php
@@ -4,10 +4,74 @@ declare(strict_types=1);
 
 namespace Klimick\DoctrinePhpMapping\Mapping;
 
+use Klimick\DoctrinePhpMapping\Field\EmbedField;
+use Klimick\DoctrinePhpMapping\Field\Field;
+use Klimick\DoctrinePhpMapping\Field\IdField;
+use Klimick\DoctrinePhpMapping\Field\ManyToManyField;
+use Klimick\DoctrinePhpMapping\Field\ManyToOneField;
+use Klimick\DoctrinePhpMapping\Field\OneToOneField;
+use Klimick\DoctrinePhpMapping\Field\OwningSide;
+use Klimick\DoctrinePhpMapping\Field\InverseSide;
+
 /**
  * @template-covariant TEntity of object
- * @extends EntityMapping<TEntity>
  */
-abstract class MappedSuperclassMapping extends EntityMapping
+abstract class MappedSuperclassMapping
 {
+    final private function __construct()
+    {
+    }
+
+    /**
+     * @return class-string<TEntity>
+     */
+    abstract public static function forClass(): string;
+
+    /**
+     * @return array<non-empty-literal-string, IdField | Field>
+     */
+    public static function fields(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array<non-empty-literal-string, EmbedField>
+     */
+    public static function embedded(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array<non-empty-literal-string, ManyToManyField | OwningSide\ManyToManyField | InverseSide\ManyToManyField>
+     */
+    public static function manyToMany(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array<non-empty-literal-string, ManyToOneField | OwningSide\ManyToOneField>
+     */
+    public static function manyToOne(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array<non-empty-literal-string, InverseSide\OneToManyField>
+     */
+    public static function oneToMany(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array<non-empty-literal-string, OneToOneField | OwningSide\OneToOneField | InverseSide\OneToOneField>
+     */
+    public static function oneToOne(): array
+    {
+        return [];
+    }
 }
